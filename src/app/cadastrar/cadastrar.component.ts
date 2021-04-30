1394ea00e7348c6949ad9fe557942b2eca9b7e2a
larissa4lves <p4ndap0wer24@gmail.com>
2021-04-30 17:01:23 -0300

    exercicio de consumo-parte1

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar.component.css']
 })
 export class CadastrarComponent implements OnInit {
+user: User = new User
+confirmarSenha: string
+tipoUsuario: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+    
+  ngOnInit() {
+    window.scroll(0,0)
+  }
 
-  ngOnInit(): void {
+  confirmSenha(event: any){
+    this.confirmSenha= event.target.value
+  }
+
+  tipoUser(event: any){
+  this.tipoUsuario = event.target.value
+}
+
+  cadastrar(){
+    this.user.tipo = this.tipoUsuario
+    
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas.')
+    } else {
+      this.authService.cadastrar(this.user).subscribe ((resp: User) =>{
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+    }
   }
 
 }
